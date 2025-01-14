20b36704d4d67327d533f9882b97d72ae0a94e4c
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2025-01-14 13:46:01 -0300

    create global error handler and first route

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,12 +1,38 @@
-import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+// Error handler global
+server.setErrorHandler((error, request, reply) => {
+  // Erros esperados a aplicação deve tratar
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  // Erros inesperados a aplicação envia para ferramentas de monitoramento
+  console.log(error)
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
 
 server.register(fastifyCors, {
   origin: '*',
 })
+
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('Server is running on port 3333')
