91892f13d3d45acb9f62414b184300dd31442040
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2025-01-17 10:16:47 -0300

    create function to export uploads #20

---
+++ src/app/functions/export-uploads.spec.ts
@@ -0,0 +1,21 @@
+import { randomUUID } from 'node:crypto'
+import { makeUpload } from '@/test/factories/make-upload'
+import { describe, it } from 'vitest'
+import { exportUploads } from './export-uploads'
+
+describe('Export uploads', () => {
+  it('should be able to export uploads', async () => {
+    const namePattern = randomUUID()
+
+    const upload1 = await makeUpload({ name: `${namePattern}.webp` })
+    const upload2 = await makeUpload({ name: `${namePattern}.webp` })
+    const upload3 = await makeUpload({ name: `${namePattern}.webp` })
+    const upload4 = await makeUpload({ name: `${namePattern}.webp` })
+    const upload5 = await makeUpload({ name: `${namePattern}.webp` })
+
+    // system under test
+    const sut = await exportUploads({
+      searchQuery: namePattern,
+    })
+  })
+})
