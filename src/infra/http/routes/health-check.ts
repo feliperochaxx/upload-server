c3540ebf7761bfd65b5d1f4636b31c966ad8faf7
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-02-21 08:35:47 -0300

    update health check message

--- src/infra/http/routes/health-check.ts
+++ src/infra/http/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
 export const healthCheckRoute: FastifyPluginAsyncZod = async server => {
   server.get('/health', async (request, reply) => {
-    await reply.status(200).send({ message: 'OK!' })
+    await reply.status(200).send({ message: 'Tudo OK agora no ECS' })
   })
 }
